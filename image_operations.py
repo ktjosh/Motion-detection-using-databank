ac75bf83adfc173dc8090f0aad2f7a0ebc6455c9
ktjosh <33245142+ktjosh@users.noreply.github.com>
2018-04-22 16:28:54 -0400

    code added for background subtraction, binarizing and Blob detection, bounding box

--- image_operations.py
+++ image_operations.py
@@ -106,26 +106,129 @@ def VideoCapture(dct):
     # depends on the future design
 
 
-def BlobDetection(Binary_image):
+# def BlobDetection(Binary_image):
+#     """
+#     Takes input as Binary image and performs blob detection
+#     :param Binary_image: a black and white binary image
+#     :return:
+#     """
+#
+#     MINArea = 5000
+#     Imopen_image = imopen(Binary_image)
+#     Bwlabel, num_regions = label(Imopen_image, neighbors=None, background=0,
+#                                  return_num=True, connectivity=2)
+#
+#     # print(num_regions)
+#     Area_region = regionprops(Bwlabel, intensity_image=None, cache=True)
+#
+#     bbox_ccordinates = []
+#     for labels in range(1, num_regions):
+#         if Area_region[labels].area > MINArea:
+#             bbox_ccordinates.append(Area_region[labels].bbox)
+#
+#             # bbox_coordinates is a list containing tuples
+#             # each tuple has (x1,y1,x2,y2) coordinates of a bounding box
+#             # depends on the future design
+
+def Background_Subtraction_And_Binarize(input_image_dct):
     """
-    Takes input as Binary image and performs blob detection
-    :param Binary_image: a black and white binary image
+    fucntion takes input as a dictionary contianing list of gray scled images and background models
+    it performs 'background subtraction' and stores it in a list.
+    After that each background subtracted image is binarized using utsu's thresholding
+    :param input_image_dct: dictionary contaning two lists 1.gray scaled images in 'uint8' format and
+    background model in 'float' format.
+    :return: list containng background subtracked images
+    """
+
+    Buffer_output =[]
+    output=[]
+    dummy =[]
+    images=[]
+    back_model =[]
+    back_model_size = 5
+    for keys in input_image_dct:
+        if len(dummy)!=0:
+            if len(dummy)> len(input_image_dct[keys]):
+                images = dummy
+                back_model = input_image_dct[keys]
+            else:
+                back_model=dummy
+                images = input_image_dct[keys]
+            break
+        dummy = input_image_dct[keys]
+
+    # print(len(images),len(back_model))
+    back_model_idx=0
+    for idx in range(5,len(images)):
+        img = images[idx]
+        img = scaletofloat(img)
+        if (idx+1)% back_model_size ==0:
+            back_model_idx+=1
+        back_sub = cv2.absdiff(img, back_model[back_model_idx])
+        Buffer_output.append(back_sub)
+
+    for back_sub_images in Buffer_output:
+        binarized_image = GRAY2BINARY(back_sub_images)
+        output.append(binarized_image)
+
+    return output
+
+
+
+def Blob_Detection_and_Bounding_box(input_img_dct):
+    """
+
+    :param input_img_dct:
     :return:
     """
+    structural_el = np.ones((5, 5), np.float)
+    MINArea=5000
+    colored_images=[]
+    binarized_images =[]
 
-    MINArea = 5000
-    Imopen_image = imopen(Binary_image)
-    Bwlabel, num_regions = label(Imopen_image, neighbors=None, background=0,
-                                 return_num=True, connectivity=2)
+    for keys in input_img_dct:
+        if len(input_img_dct[keys][0].shape)==3:
+            colored_images= input_img_dct[keys]
+            print("found colored images")
+        else:
+            binarized_images=input_img_dct[keys]
+            print("found binarized images")
+    difference_in_frames = len(colored_images)-len(binarized_images)
 
-    # print(num_regions)
-    Area_region = regionprops(Bwlabel, intensity_image=None, cache=True)
+    cv2.namedWindow('OUTPUT')
+    # print(len(colored_images))
+    # print(len(binarized_images))
+    for img_idx in range (len(colored_images)):
+        camera_image= colored_images[img_idx]
 
-    bbox_ccordinates = []
-    for labels in range(1, num_regions):
-        if Area_region[labels].area > MINArea:
-            bbox_ccordinates.append(Area_region[labels].bbox)
+        if(img_idx>=difference_in_frames):
+            # performing morphlogical opening on the image
 
-            # bbox_coordinates is a list containing tuples
-            # each tuple has (x1,y1,x2,y2) coordinates of a bounding box
-            # depends on the future design
+            imopen_image = cv2.morphologyEx(binarized_images[img_idx-difference_in_frames]
+                                            , cv2.MORPH_OPEN, structural_el)
+            # finding region in an image
+            Bwlabel, num_regions = label(imopen_image, neighbors=None, background=0, return_num=True, connectivity=2)
+
+            # print(num_regions)
+            Area_region = regionprops(Bwlabel, intensity_image=None, cache=True)
+
+            bbox_ccordinates = []
+            for labels in range(1, num_regions):
+                if Area_region[labels].area > MINArea:
+                    bbox_ccordinates.append(Area_region[labels].bbox)
+
+            for points in bbox_ccordinates:
+                cv2.rectangle(camera_image, (points[0], points[1]), (points[2], points[3]), (0, 255, 0), 3)
+
+        cv2.imshow('OUTPUT',camera_image)
+        cv2.waitKey(4)
+
+def GRAY2BINARY(input_image):
+    """
+    The method uses otsu's method to find the threshold to convert it to a binary image.
+    :param input_image:the input image will be a 2-D grayscale image
+    :return:
+    """
+    thresh = threshold_otsu(input_image)
+    ret, binarized = cv2.threshold(input_image, thresh, 1, cv2.THRESH_BINARY)
+    return binarized
--- image_testing.py
+++ image_testing.py
@@ -39,11 +39,13 @@ def testing():
     output=[]
     path = "Experiment.mp4"
     Vid = cv2.VideoCapture(path)
+    colored_images =[]
     while (Vid.isOpened()):
         # frame by frame is read here
         rt, frame = Vid.read()
         # grayscale
         if rt != False:
+            colored_images.append(frame)
             frame =  cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             output.append(frame)
         else:
@@ -52,7 +54,19 @@ def testing():
     print("done with video capture")
     dict ={}
     dict[1]=output
-    Background_Model(dict)
+
+    back_model=Background_Model(dict)
+    print("done with background model",len(back_model))
+    dict[2]= back_model
+
+    sub_images=Background_Subtraction_And_Binarize(dict)
+    print("done with subtraction and binarization")
+    dict2={}
+    dict2[1]=colored_images
+    dict2[2]=sub_images
+    Blob_Detection_and_Bounding_box(dict2)
+
+
 
 
 """
@@ -63,7 +77,8 @@ imopen : morphological operations
 VideoCapture: Captures the frame by frame from the prerecorded video
 Blob detection: Detects blobs in binary image
 Background Model
-Background Subtraction
+Background Subtraction and Binarized
+Blob_detection and Bounding box
 """
 
 def GrayScale(input_image_dct):
@@ -81,7 +96,7 @@ def GrayScale(input_image_dct):
     return output
 
 
-def imBinarize(input_image):
+def GRAY2BINARY(input_image):
     """
     The method uses otsu's method to find the threshold to convert it to a binary image.
     :param input_image:the input image will be a 2-D grayscale image
@@ -183,16 +198,18 @@ def Background_Model(input_image_dct):
     #     cv2.waitKey(2)
     return output
 
-def Background_Subtraction(input_image_dct):
+def Background_Subtraction_And_Binarize(input_image_dct):
     """
     fucntion takes input as a dictionary contianing list of gray scled images and background models
-    it performs background subtraction and stores it in a list
+    it performs 'background subtraction' and stores it in a list.
+    After that each background subtracted image is binarized using utsu's thresholding
     :param input_image_dct: dictionary contaning two lists 1.gray scaled images in 'uint8' format and
-    background model in 'float' format
+    background model in 'float' format.
     :return: list containng background subtracked images
     """
 
-    output =[]
+    Buffer_output =[]
+    output=[]
     dummy =[]
     images=[]
     back_model =[]
@@ -208,18 +225,74 @@ def Background_Subtraction(input_image_dct):
             break
         dummy = input_image_dct[keys]
 
-    #back_sub = cv2.absdiff(b_lab_image, background_model)
-
+    # print(len(images),len(back_model))
     back_model_idx=0
     for idx in range(5,len(images)):
         img = images[idx]
         img = scaletofloat(img)
-        if (back_model_idx+1)% back_model_size ==0:
+        if (idx+1)% back_model_size ==0:
             back_model_idx+=1
         back_sub = cv2.absdiff(img, back_model[back_model_idx])
-        output.append(back_sub)
+        Buffer_output.append(back_sub)
+
+    for back_sub_images in Buffer_output:
+        binarized_image = GRAY2BINARY(back_sub_images)
+        output.append(binarized_image)
 
     return output
+
+
+
+def Blob_Detection_and_Bounding_box(input_img_dct):
+    """
+
+    :param input_img_dct:
+    :return:
+    """
+    structural_el = np.ones((5, 5), np.float)
+    MINArea=5000
+    colored_images=[]
+    binarized_images =[]
+
+    for keys in input_img_dct:
+        if len(input_img_dct[keys][0].shape)==3:
+            colored_images= input_img_dct[keys]
+            print("found colored images")
+        else:
+            binarized_images=input_img_dct[keys]
+            print("found binarized images")
+    difference_in_frames = len(colored_images)-len(binarized_images)
+
+    cv2.namedWindow('OUTPUT')
+    # print(len(colored_images))
+    # print(len(binarized_images))
+    for img_idx in range (len(colored_images)):
+        camera_image= colored_images[img_idx]
+
+        if(img_idx>=difference_in_frames):
+            # performing morphlogical opening on the image
+
+            imopen_image = cv2.morphologyEx(binarized_images[img_idx-difference_in_frames]
+                                            , cv2.MORPH_OPEN, structural_el)
+            # finding region in an image
+            Bwlabel, num_regions = label(imopen_image, neighbors=None, background=0, return_num=True, connectivity=2)
+
+            # print(num_regions)
+            Area_region = regionprops(Bwlabel, intensity_image=None, cache=True)
+
+            bbox_ccordinates = []
+            for labels in range(1, num_regions):
+                if Area_region[labels].area > MINArea:
+                    bbox_ccordinates.append(Area_region[labels].bbox)
+
+            for points in bbox_ccordinates:
+                cv2.rectangle(camera_image, (points[0], points[1]), (points[2], points[3]), (0, 255, 0), 3)
+
+        cv2.imshow('OUTPUT',camera_image)
+        cv2.waitKey(4)
+
+
+
 
 
 
@@ -237,6 +310,6 @@ def justread():
 def main():
     testing()
 
-    justread()
+    #justread()
 if __name__ == '__main__':
     main()
